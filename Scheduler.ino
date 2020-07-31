56670b0a8c22f3570d6635def1329c8f5220b679
shuvangkar <sshuvo93@gmail.com>
2020-07-31 22:52:06 +0600

    led blink testing for whole day

--- Scheduler.ino
+++ Scheduler.ino
@@ -2,6 +2,9 @@
 #include <TimerOne.h>
 
 uint32_t milliSec = 0;
+#define LED 13
+
+
 void func1()
 {
   Serial.println(F("Task One Done"));
@@ -16,21 +19,31 @@ void func3()
 {
   Serial.println(F("Task Three Done"));
 }
+void led_blink()
+{
+  Serial.println(F("LED BLINK"));
+  digitalWrite(LED,HIGH);
+  delay(500);
+  digitalWrite(LED,LOW);
+}
 
 Task task1(2000, &func1);
 Task taks2(2000, &func2);
 Task task3(3000, &func3);
+Task task4(1000, &led_blink);
 
 Scheduler scheduler;
 void setup()
 {
   Serial.begin(115200);
 
+  pinMode(LED,OUTPUT);
   Timer1.initialize(1000);
   Timer1.attachInterrupt(timeCounter);
   scheduler.addTask(&task1);
   scheduler.addTask(&taks2);
   scheduler.addTask(&task3);
+  scheduler.addTask(&task4);
 //  scheduler.begin(ms);
      scheduler.begin();
   Serial.println(F("Setup Done"));
